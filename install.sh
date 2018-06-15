c671717cee9fc5e26c870db1dc727dee1458a788
Amit Lavon <fluhus@users.noreply.github.com>
2018-06-15 14:31:48 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-# Installs sketchify into GIMP.
-echo "To install run the following command:"
-echo "$(gimptool-2.0 -n --install get_pixels.c)" -std=c99 "&&" "$(gimptool-2.0 -n --install-script sketchify.py)/../plug-ins/"
-
